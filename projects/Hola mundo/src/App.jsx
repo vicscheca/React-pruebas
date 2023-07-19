1b9f8bc1aa1e067835240c22289d63808bd747f9
vchec <vcheca1994@gmail.com>
2023-07-19 17:50:21 +0200

    Commit 1
    Copia de los logos de twitter simple

---
+++ projects/Hola mundo/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+export function App () {
+    return (
+        <article>
+            <header>
+            <img alt="Avatar Pikavics" src="https://unavatar.io/pikavics" />
+                <div>
+                    <strong>Pikachu</strong>
+                    <span>@Pikavics</span>
+                </div>
+            </header>
+
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+    }
